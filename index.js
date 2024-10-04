9661bd7b339d7fa99d5ce337db0d3f34951e0915
Akhil M Mathew <iamakhil99@gmail.com>
2024-10-05 03:34:58 +0530

    Updated the responsiveness of info page

--- index.js
+++ index.js
@@ -3,30 +3,30 @@ import bodyParser from "body-parser";
 import axios from "axios";
 
 const app = express();
-const port = 3000;
-const API_URL = "http://api.weatherapi.com/v1/current.json?q="
+const port = 10000;
+const API_URL = "http://api.weatherapi.com/v1/current.json?q=";
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+  res.render("index.ejs");
 });
 
 app.get("/contact", (req, res) => {
-    res.render("contact.ejs");
+  res.render("contact.ejs");
 });
 
 app.get("/location", async (req, res) => {
-    const API_KEY = req.query.key;
-    const config = {
-        headers: { key: `${API_KEY}` }
-    }
-    const location = req.query.location;
-    const result = await axios.get(API_URL + location + "&aqi=yes", config);
-    res.render("info.ejs", { content: result.data });
+  const API_KEY = req.query.key;
+  const config = {
+    headers: { key: `${API_KEY}` },
+  };
+  const location = req.query.location;
+  const result = await axios.get(API_URL + location + "&aqi=yes", config);
+  res.render("info.ejs", { content: result.data });
 });
 
 app.listen(port, () => {
-    console.log(`Server Started on port ${port}`);
+  console.log(`Server Started on port ${port}`);
 });
